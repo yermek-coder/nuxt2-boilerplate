97db613af2c115595e2cb34c78711463f1d1faea
yermek-dev <mynameisermek@gmail.com>
2022-05-10 12:46:12 +0600

    idk

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
+  server: {
+    host: process.env.HOST || 'localhost',
+  },
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
