b38f27b7a0b0e42157689f5b2d0cd08f513635a1
yermek-dev <mynameisermek@gmail.com>
2022-05-10 19:06:50 +0600

    started building components

---
+++ components/base/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="product-card">
+    <div class="picture">
+      <div class="photo"></div>
+      <div class="background"></div>
+    </div>
+    <div class="description">
+      <h5 class="title"></h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
