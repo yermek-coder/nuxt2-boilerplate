ab99f2e2767952de270192814fef84bf75be9a45
yermek-dev <mynameisermek@gmail.com>
2022-05-24 15:10:10 +0600

    finished single date picker, works with inactive dates and scoped slots

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <h1>Boilerplate</h1>
     <base-date-input v-model="singleDate"></base-date-input>
     <base-date-picker v-model="singleDate" :inactive-dates="inactiveDates">
-      <template #2022-05-09="{ day }">{{day.value}}slots</template>
+      <template #2022-05-09="{ day }">
+        <span class="slot">{{day.value}} slot</span>
+      </template>
     </base-date-picker>
   </div>
 </template>
@@ -25,5 +27,14 @@ export default {
 <style lang="scss" scoped>
 .bg {
   background: rgb(153, 153, 153);
+}
+.slot {
+  border-bottom: 2px solid red;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  font-size: 8px;
 }
 </style>
